6e374c041caea848519c4eaa6577a30e6f0f4c59
9kmmr <tunglam195@gmail.com>
2021-07-16 17:16:33 +0700

    add temp data for login section

--- src/screens/auth/ForgotPassword.tsx
+++ src/screens/auth/ForgotPassword.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
-import { View } from 'react-native';
+
+import { View, StyleSheet,  Dimensions, Image, Text, StatusBar } from 'react-native';
+import { Button } from '../../components';
+import { ThemeContext } from '../../theme'
+import { navigation } from '../../navigations/RootNavigation';
+const { width } = Dimensions.get("window");
 
 function ForgotPassword() {
     return (
-        <View></View>
+        <View style={styles.container}>
+            <StatusBar barStyle="light-content" />
+            <View  style={styles.headerContainer}>
+                
+            </View>
+            <View style={styles.mainContent}>
+                <View style={styles.footer}>
+
+                </View>
+            </View>
+
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white'
+    },
+    headerContainer: {
+        backgroundColor: "green",
+        borderBottomLeftRadius: 75,
+        overflow: 'hidden',
+        height: width * 2 / 3,
+    },
+    mainContent: {
+        flex: 1,
+        overflow: "hidden"
+    },
+    footer: {        
+        flex: 1,
+        borderRadius: 75,
+        borderTopLeftRadius: 0,
+        backgroundColor: "white"
+    }
+
+})
 
 export default ForgotPassword;
